3cc20d66c763d76a74b5fe7eea0643eeae1b3ed8
tetra0717 <tetraardor0717@gmail.com>
2025-11-23 02:13:32 +0900

    アニメーションのもっさりを改善

--- client/app/mobile/_components/combo.tsx
+++ client/app/mobile/_components/combo.tsx
@@ -1,35 +1,20 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import styles from "./combo.module.css";
 
 export function Combo(props: { combo: number }) {
-  const [prevCombo, setPrevCombo] = useState(0);
-  const [animate, setAnimate] = useState(false);
-
   const { combo } = props;
-
-  useEffect(() => {
-    if (combo > prevCombo) {
-      setAnimate(true);
-      const timeout = setTimeout(() => setAnimate(false), 500);
-      setPrevCombo(combo);
-      return () => clearTimeout(timeout);
-    } else {
-      setPrevCombo(combo);
-    }
-  }, [combo, prevCombo]);
-
-  if (combo === 0) return null;
 
   return (
     <div className={styles.container}>
       <span
-        className={`${styles.comboNumber} ${animate ? styles.animate : ""}`}
+        className={`${styles.comboNumber} ${combo > 0 ? styles.animate : ""}`}
       >
         {combo}
       </span>
-      <span className={`${styles.comboText} ${animate ? styles.animate : ""}`}>
+      <span
+        className={`${styles.comboText} ${combo > 0 ? styles.animate : ""}`}
+      >
         COMBO
       </span>
     </div>
